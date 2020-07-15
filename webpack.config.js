141ee54aa96009df4602350dcc1e1f0a18cb3d53
Oğuzcan YAVUZ <oguzcanyavuz321@gmail.com>
2020-07-15 16:04:14 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const isLocal = slsw.lib.webpack.isLocal;
+
+module.exports = {
+  mode: isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  externals: [nodeExternals()],
+  devtool: isLocal ? 'cheap-module-eval-source-map' : 'source-map',
+  resolve: {
+    extensions: [ '.js', '.jsx', '.json', '.ts', '.tsx' ]
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  },
+  target: 'node',
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.(tsx?)$/,
+        loader: 'ts-loader',
+        exclude: [
+          [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '.serverless'),
+            path.resolve(__dirname, '.webpack'),
+            path.resolve(__dirname, '.dynamodb'),
+          ],
+        ],
+        options: {
+          transpileOnly: true,
+          experimentalWatchApi: true,
+        },
+      },
+    ],
+  },
+  // plugins: [
+  //   // new ForkTsCheckerWebpackPlugin({
+  //   //   eslint: true,
+  //   //   eslintOptions: {
+  //   //     cache: true
+  //   //   }
+  //   // })
+  // ],
+  plugins: [],
+  optimization: {
+    minimize: true
+  }
+};
