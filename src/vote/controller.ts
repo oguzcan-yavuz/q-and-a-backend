9cb0286f39e0cd4f0ea9449db26e30cd3742e052
Oğuzcan YAVUZ <oguzcanyavuz321@gmail.com>
2020-08-08 20:12:03 +0300

    refactor(votes): refactor votes to retrieve userId from authorizer context
    fix #8

--- src/vote/controller.ts
+++ src/vote/controller.ts
@@ -1,12 +1,19 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyWithCognitoAuthorizerHandler } from 'aws-lambda';
 import * as VoteService from './service';
 import * as HttpStatus from 'http-status-codes';
 import { Vote } from './Vote';
 import { createProxyResult } from '../util';
 
-export const voteQuestion: APIGatewayProxyHandler = async (event) => {
+export const voteQuestion: APIGatewayProxyWithCognitoAuthorizerHandler = async (event) => {
   const questionId = event.pathParameters!.questionId as Vote['questionId'];
-  const { type, userId } = JSON.parse(event.body!) as Pick<Vote, 'type' | 'userId'>;
+  const { type } = JSON.parse(event.body!) as Pick<Vote, 'type'>;
+  const {
+    requestContext: {
+      authorizer: {
+        claims: { sub: userId },
+      },
+    },
+  } = event;
   const voteResponse = await VoteService.voteQuestion({
     questionId,
     userId,
@@ -16,9 +23,15 @@ export const voteQuestion: APIGatewayProxyHandler = async (event) => {
   return createProxyResult(HttpStatus.OK, voteResponse);
 };
 
-export const getVotesOfCurrentUser: APIGatewayProxyHandler = async (event) => {
+export const getVotesOfCurrentUser: APIGatewayProxyWithCognitoAuthorizerHandler = async (event) => {
   const meetingId = event.pathParameters!.meetingId as Vote['meetingId'];
-  const userId = '' as Vote['userId']; // TODO: get userId from jwt claim
+  const {
+    requestContext: {
+      authorizer: {
+        claims: { sub: userId },
+      },
+    },
+  } = event;
   const userVotes = await VoteService.getVotesOfCurrentUser({ meetingId, userId });
 
   return createProxyResult(HttpStatus.OK, userVotes);
